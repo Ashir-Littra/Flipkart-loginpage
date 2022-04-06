07a3ec396a2b55ec55fbe5b37010e033b4968e79
Ashir-Littra <ashirnbasheer@gmail.com>
2022-04-06 14:18:39 +0530

    button component completed and finished

---
+++ src/Components/Button/Button.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './button.css'
+
+const Button = () => {
+  return (
+    <div className="button-container">
+      <button className="btn">Continue</button>
+    </div>
+  )
+}
+
+export default Button
