bbdde8c3ae06a0659c0af65afdec89ac02c264b1
Ashir-Littra <ashirnbasheer@gmail.com>
2022-04-06 12:34:33 +0530

    header is finished

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './header.css'
+import logo from './logo.png'
+import close from './close.png'
+
+const Header = () => {
+  return (
+    <div className="header-container">
+        <div className="header-closing">
+        <img  src={close} alt='x' />   
+        </div>
+      <div className="header-title">
+          <img className="logo" src={logo} alt='flipkart' />
+      </div>
+    </div>
+  )
+}
+
+export default Header
